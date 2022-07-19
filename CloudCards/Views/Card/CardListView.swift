27eb5c488492113ad3dbb66b840100098a25b34d
Simon Rofe <simon.rofe@ieee.org>
2022-07-20 08:12:16 +1000

    Add card list view for starter project

---
+++ CloudCards/Views/Card/CardListView.swift
@@ -0,0 +1,51 @@
+//
+//  CardListView.swift
+//  CloudCards
+//
+//  Created by Simon Rofe on 20/7/2022.
+//
+
+import SwiftUI
+
+struct CardListView: View {
+    @ObservedObject var model = Model()
+    @State var showForm = false
+    @State var showUserView = false
+    @State var showSignInView = false
+
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                ScrollView(.vertical) {
+                    VStack {
+                        // TODO: Add cards here!
+                    }
+                    .frame(width: geometry.size.width)
+                }
+            }
+            .sheet(isPresented: $showForm) {
+                NewCardForm(cardListViewModel: model)
+            }
+            .navigationTitle("Cloud Cards")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading: Button { showUserView = true }
+                label: {
+                    Image(systemName: "person.fill")
+                        .font(.title)
+                },
+                trailing: Button { showForm.toggle() }
+                label: {
+                    Image(systemName: "plus")
+                        .font(.title)
+                }
+            )
+        }
+    }
+}
+
+struct CardListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardListView()
+    }
+}
