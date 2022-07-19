835af366548c3dd1a25abd3adc77b3c0062020a8
Simon Rofe <simon.rofe@ieee.org>
2022-07-19 11:50:58 +1000

    Initial Commit

---
+++ CloudCards/CloudCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudCardsApp.swift
+//  CloudCards
+//
+//  Created by Simon Rofe on 19/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct CloudCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
