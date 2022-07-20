f7d4dcc5e2c097d377b0e8a86d1d1f4cf862a8e3
Simon Rofe <simon.rofe@ieee.org>
2022-07-20 16:55:33 +1000

    Fetch cards from Firestore repository

--- CloudCards/Repositories/CardRepository.swift
+++ CloudCards/Repositories/CardRepository.swift
@@ -4,3 +4,30 @@
 //
 //  Created by Simon Rofe on 19/7/2022.
 //
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine
+
+class CardRepository: ObservableObject {
+    private let store = Firestore.firestore()
+    private let path = "cards"
+    @Published var cards: [Card] = []
+
+    init() {
+        get()
+    }
+
+    func get() {
+        store.collection(path)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error fetting cards: \(error.localizedDescription)")
+                    return
+                }
+                self.cards = querySnapshot?.documents.compactMap { document in
+                    try? document.data(as: Card.self)
+                } ?? []
+            }
+    }
+}
