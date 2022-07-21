0f091f8f499153a72e641cac22071cc92502e4d8
Simon Rofe <simon.rofe@ieee.org>
2022-07-21 12:46:52 +1000

    Refactor class method locations

--- CloudCards/Repositories/CardRepository.swift
+++ CloudCards/Repositories/CardRepository.swift
@@ -14,6 +14,23 @@ class CardRepository: ObservableObject {
     private let path = "cards"
     @Published var cards: [Card] = []
 
+    init() {
+        get()
+    }
+
+    func get() {
+        store.collection(path)
+            .addSnapshotListener { querySnapshot, error in
+                if let error = error {
+                    print("Error getting cards: \(error.localizedDescription)")
+                    return
+                }
+                self.cards = querySnapshot?.documents.compactMap { document in
+                    try? document.data(as: Card.self)
+                } ?? []
+            }
+    }
+
     func add(_ card: Card) {
         do {
             _ = try store.collection(path).addDocument(from: card)
@@ -38,22 +55,5 @@ class CardRepository: ObservableObject {
                 print("Unable to remove card: \(error.localizedDescription)")
             }
         }
-    }
-
-    init() {
-        get()
-    }
-
-    func get() {
-        store.collection(path)
-            .addSnapshotListener { querySnapshot, error in
-                if let error = error {
-                    print("Error fetting cards: \(error.localizedDescription)")
-                    return
-                }
-                self.cards = querySnapshot?.documents.compactMap { document in
-                    try? document.data(as: Card.self)
-                } ?? []
-            }
     }
 }
