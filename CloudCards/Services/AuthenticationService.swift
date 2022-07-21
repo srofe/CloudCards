e86694ece10b18316c85c1f64da9dbc00390c461
Simon Rofe <simon.rofe@ieee.org>
2022-07-21 12:40:40 +1000

    Implement authentication service

--- CloudCards/Services/AuthenticationService.swift
+++ CloudCards/Services/AuthenticationService.swift
@@ -4,3 +4,30 @@
 //
 //  Created by Simon Rofe on 19/7/2022.
 //
+
+import Firebase
+
+class AuthenticationService: ObservableObject {
+    @Published var user: User?
+    private var authenticationStateHandle: AuthStateDidChangeListenerHandle?
+
+    init() {
+        addListeners()
+    }
+
+    static func signIn() {
+        if Auth.auth().currentUser == nil {
+            Auth.auth().signInAnonymously()
+        }
+    }
+
+    private func addListeners() {
+        if let handle = authenticationStateHandle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+        authenticationStateHandle = Auth.auth()
+            .addStateDidChangeListener { _, user in
+                self.user = user
+            }
+    }
+}
